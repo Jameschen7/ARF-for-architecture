86166969a26bf82e918cf887a333e285f7b147c9
Ubuntu <ubuntu@ip-172-31-0-109.us-east-2.compute.internal>
2023-04-10 20:52:33 +0000

    edit create_env and config for low reso

--- create_env.sh
+++ create_env.sh
@@ -1,4 +1,9 @@
 conda env create -f environment.yml && conda activate arf-svox2
 # install customized cuda kernels
-pip install . --upgrade --use-feature=in-tree-build
+# pip install . --upgrade --use-feature=in-tree-build
+# pip install icecream
+
+pip install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0 --extra-index-url https://download.pytorch.org/whl/cu113
+# pip install numpy==1.20
+pip install . --upgrade 
 pip install icecream
--- opt/try_tnt.sh
+++ opt/try_tnt.sh
@@ -8,18 +8,28 @@ data_dir=../data/${data_type}/${SCENE}
 style_img=../data/styles/${STYLE}.jpg
 
 
+# if [[ ! -f "${ckpt_svox2}/ckpt.npz" ]]; then
+#     python opt.py -t ${ckpt_svox2} ${data_dir} \
+#                 -c configs/tnt.json
+# fi
 if [[ ! -f "${ckpt_svox2}/ckpt.npz" ]]; then
     python opt.py -t ${ckpt_svox2} ${data_dir} \
-                -c configs/tnt.json
+                -c configs/tnt_low_reso.json 
 fi
 
 
+# python opt_style.py -t ${ckpt_arf} ${data_dir} \
+#                 -c configs/tnt_fixgeom.json  \
+#                 --init_ckpt ${ckpt_svox2}/ckpt.npz \
+#                 --style ${style_img} \
+#                 --mse_num_epoches 1 --nnfm_num_epoches 10 \
+#                 --content_weight 5e-3
 python opt_style.py -t ${ckpt_arf} ${data_dir} \
-                -c configs/tnt_fixgeom.json  \
+                -c configs/tnt_fixgeom_low_reso.json  \
                 --init_ckpt ${ckpt_svox2}/ckpt.npz \
                 --style ${style_img} \
                 --mse_num_epoches 1 --nnfm_num_epoches 10 \
                 --content_weight 5e-3
 
 python render_imgs.py ${ckpt_arf}/ckpt.npz ${data_dir} \
-                --render_path --no_imsave
+                --render_path #--no_imsave
